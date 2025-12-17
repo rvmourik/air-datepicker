9dcbabbf0d509c74a33aafe0105cf64e20fa0f63
Robbert van Mourik <rvmourik@users.noreply.github.com>
2025-12-17 13:46:48 +0100

    Add button attributes for today and clear presets
    A HTML button type will be interpret as a submit when the type is ommited.

--- src/buttonPresets.js
+++ src/buttonPresets.js
@@ -2,9 +2,15 @@ export default {
     today: {
         content: dp => dp.locale.today,
         onClick: dp => dp.setViewDate(new Date()),
+        attrs: {
+            type: 'button'
+        }
     },
     clear: {
         content: dp => dp.locale.clear,
-        onClick: dp => dp.clear()
+        onClick: dp => dp.clear(),
+        attrs: {
+            type: 'button'
+        }
     }
 };
